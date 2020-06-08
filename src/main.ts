mymindstorm/matrix-appservice-mumble

commit 35421c4960c0f0ebd525d174ce4959da46ebda48

fix #5 - Types for matrix-appservice-bridge

src/main.ts
@@ -1,12 +1,13 @@
 import Murmur from'./Murmur';
-// @ts-ignore
 import {Cli, Bridge, AppServiceRegistration} from 'matrix-appservice-bridge';
 import { MurmurConfig } from './types';
 
 async function main() {
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/45345
+  // @ts-ignore
   new Cli({
     registrationPath: 'mumble-registration.yaml',
-    generateRegistration(reg: any, callback: (reg: any) => void) {
+    generateRegistration(reg, callback: (reg: any) => void) {
       reg.setId(AppServiceRegistration.generateToken());
       reg.setHomeserverToken(AppServiceRegistration.generateToken());
       reg.setAppServiceToken(AppServiceRegistration.generateToken());
@@ -24,16 +25,17 @@ async function main() {
       if (!murmur.client) {
         console.log('Connection error.');
         process.exit(1);
-        return;
       }
       console.log('Connetion to Murmur established!');
 
       const bridge = new Bridge({
         homeserverUrl: config.homeserverURL,
         domain: config.domain,
+        // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/45345
+        // @ts-ignore
         registration: 'mumble-registration.yaml',
         controller: {
-          onEvent: function(request: any, context: any) {
+          onEvent: function(request: any, _context: any) {
             const event = request.getData();
             if (event.type !== 'm.room.message' ||
                 !event.content || event.room_id !== config.matrixRoom) {

commit 38d49969a358d20b6884c8a523cefe472bb48c62

Fix build failure after matrix-appservice-bridge types
Since commit 35421c4960c0 ("fix #5 - Types for
matrix-appservice-bridge"), the build fails with the following error:

  src/main.ts:55:68 - error TS2345: Argument of type 'null' is not assignable to parameter of type 'string | undefined'.

  55       murmur.setMatrixClient(bridge.getClientFactory().getClientAs(null));
                                                                        ~~~~

Pass no parameter to getClientAs() since apparently null isn't
permissible any longer.

Fixes: 35421c4960c0 ("fix #5 - Types for matrix-appservice-bridge")

src/main.ts
@@ -52,7 +52,7 @@ async function main() {
       console.log('Matrix-side listening on port %s', port);
       await murmur.setupCallbacks(bridge, config);
       bridge.run(port, config);
-      murmur.setMatrixClient(bridge.getClientFactory().getClientAs(null));
+      murmur.setMatrixClient(bridge.getClientFactory().getClientAs());
       return;
     },
   }).run();